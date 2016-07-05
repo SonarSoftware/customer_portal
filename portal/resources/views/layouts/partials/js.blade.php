d6129fbd8956b0fc2dde8ad46c23a05c09497c46
Sonar Software <simon@sonar.software>
2016-07-05 13:45:19 -0500

    Simplify this include, since assets/secure_asset is not used elsewhere. Load moment on each page.

--- portal/resources/views/layouts/partials/js.blade.php
+++ portal/resources/views/layouts/partials/js.blade.php
@@ -4,10 +4,14 @@
         thousandsSeparator: '{{Config::get("customer_portal.thousands_separator")}}',
         decimalSeparator: '{{Config::get("customer_portal.decimal_separator")}}'
     };
-    moment.local('{{Config::get("app.locale")}}');
 </script>
 <script src="https://code.jquery.com/jquery-1.12.3.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-<script type="text/javascript" src="{{ secure_asset('vendor/jsvalidation/js/jsvalidation.js')}}"></script>
+<script type="text/javascript" src="vendor/jsvalidation/js/jsvalidation.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.6/Chart.bundle.min.js"></script>
+<script type="text/javascript" src="/assets/js/vendor/moment/moment.min.js"></script>
 <script type="text/javascript" src="/assets/js/global.js"></script>
+<script>
+    moment.local('{{Config::get("app.locale")}}');
+</script>
 @yield('additionalJS')
--- portal/resources/views/pages/data_usage/index.blade.php
+++ portal/resources/views/pages/data_usage/index.blade.php
@@ -50,8 +50,6 @@
     </div>
 @endsection
 @section('additionalJS')
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.6/Chart.bundle.min.js"></script>
-    <script type="text/javascript" src="/assets/js/vendor/moment/moment.min.js"></script>
     <script>
         var historicalUsage = {!! $historicalUsage !!};
     </script>
