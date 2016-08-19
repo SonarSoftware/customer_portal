9460923faf6dd951bcf13f00ed649f904021e05e
Sonar Software <simon@sonar.software>
2016-08-19 15:35:18 -0500

    Load all the JS/CSS locally to make it easier to allow access to the portal when a user has restricted access.

--- portal/resources/views/layouts/partials/head.blade.php
+++ portal/resources/views/layouts/partials/head.blade.php
@@ -7,14 +7,14 @@
     <meta name="description" content="Customer Portal">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{Config::get("customer_portal.company_name")}}</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/bootstrap-theme.min.css">
     <link rel="stylesheet" href="/assets/css/vendor/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="/assets/css/global.css">
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <script src="/assets/js/html5shiv.min.js"></script>
+    <script src="/assets/js/respond.min.js"></script>
     <![endif]-->
     <script src="/assets/lang.dist.js" type="text/javascript"></script>
     @yield('additionalCSS')
--- portal/resources/views/layouts/partials/js.blade.php
+++ portal/resources/views/layouts/partials/js.blade.php
@@ -5,10 +5,10 @@
         decimalSeparator: '{{Config::get("customer_portal.decimal_separator")}}'
     };
 </script>
-<script src="https://code.jquery.com/jquery-1.12.3.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+<script src="/assets/js/vendor/jquery/jquery-1.12.3.min.js"></script>
+<script src="/assets/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="/vendor/jsvalidation/js/jsvalidation.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.6/Chart.bundle.min.js"></script>
+<script type="text/javascript" src="/assets/js/vendor/chartjs/Chart.bundle.min.js"></script>
 <script type="text/javascript" src="/assets/js/vendor/moment/moment.min.js"></script>
 <script type="text/javascript" src="/assets/js/global.js"></script>
 <script>
