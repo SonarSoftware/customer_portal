1ed5d52da959b7ace7e3b83a436bf7735cd12a9c
Laravel Shift <shift@laravelshift.com>
2017-05-10 00:21:15 +0000

    Shift Form Requests

--- portal/app/Http/Requests/TicketReplyRequest.php
+++ portal/app/Http/Requests/TicketReplyRequest.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Foundation\Http\FormRequest;
 use App\Http\Requests\Request;
 
-class TicketReplyRequest extends Request
+class TicketReplyRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
